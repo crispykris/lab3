91d1ecd4d26f78f69fac0b0464ac703172086340
crispykris <93004806+crispykris@users.noreply.github.com>
2021-10-26 16:46:32 +0100

    blog one

---
+++ src/app.rb
@@ -0,0 +1,6 @@
+require 'sinatra'
+
+get '/' do 
+    <h1>hello internet </h1>
+    <p>welcome to my blog about my programing experience
+    and how my life is flowing. i hope i can enjoy the internet just like you</p>
